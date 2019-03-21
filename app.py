2e3df2c05368e09d2034c41737899335ba7ea587
Makayla Fox <makaylafox@mail.weber.edu>
2019-03-20 21:44:48 -0600

    push one

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+@app.route('/index')
+def welcome():
+    return render_template('index.html')  # render a template
+
+#genre routes
+@app.route('/mysteries')
+def mysteries():
+    return render_template('mysteries.html')
+
+@app.route('/fiction')
+def fiction():
+    return render_template('fiction.html')
+
+@app.route('/romance')
+def romance():
+    return render_template('romance.html')
+
+#book routes
+@app.route('/book1')
+def book1():
+    return render_template('book1.html')
+
+@app.route('/book2')
+def book2():
+    return render_template('book2.html')
+
+@app.route('/book3')
+def book3():
+    return render_template('book3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
