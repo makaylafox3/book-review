19b15f59d93719c30bed755689afe250d5b96d9a
Makayla Fox <makaylafox@mail.weber.edu>
2019-04-03 22:57:33 -0600

    push 4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime
 from flask import Flask, render_template, g, request, redirect, url_for
 
-PATH = 'db/bookData.db'
+PATH = 'db/bookData.sqlite'
 
 app = Flask(__name__)
 
@@ -39,8 +39,8 @@ def welcome():
 #genre routes
 @app.route('/mysteries')
 def mysteries():
-    mys = execute_sql('SELECT * FROM mysterys')
-    return render_template('mysteries.html', mys=mys)
+    mysterys = execute_sql('SELECT * FROM mysterys')
+    return render_template('mysteries.html', mysterys=mysterys)
     # mystery_details = {
     #     'title': 'Murder At The Palace',
     #     'author': 'by Margaret Dumas',
@@ -59,42 +59,6 @@ def romance():
     rom = execute_sql('SELECT romance.rom_id, romance.rom_title, romance.rom_author, romance.rom_description FROM romance')
     return render_template('romance.html', rom=rom)
 
-#book routes
-@app.route('/book1')
-def book1():
-    return render_template('book1.html')
-
-@app.route('/book2')
-def book2():
-    return render_template('book2.html')
-
-@app.route('/book3')
-def book3():
-    return render_template('book3.html')
-
-@app.route('/book4')
-def book4():
-    return render_template('book4.html')
-
-@app.route('/book5')
-def book5():
-    return render_template('book5.html')
-
-@app.route('/book6')
-def book6():
-    return render_template('book6.html')
-
-@app.route('/book7')
-def book7():
-    return render_template('book7.html')
-
-@app.route('/book8')
-def book8():
-    return render_template('book8.html')
-
-@app.route('/book9')
-def book9():
-    return render_template('book9.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
